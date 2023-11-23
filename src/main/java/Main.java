ae6c2e868c37c1266de53c523db351999aab68d3
slanja <97552526+slanja@users.noreply.github.com>
2023-11-23 23:12:06 +0100

    added insert sort

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int numbers[] = {2, 5, 1, 10, 3};
 
-        BubbleSort(numbers);
+        // BubbleSort(numbers);
+
+        InsertSort(numbers);
 
         PrintList(numbers);
     }
@@ -16,9 +18,8 @@ public class Main {
         for (int i = input.length; i > 0; i--) {
             // getting numbers from start of the list
             for (int j = 0; j < i - 1; j++) {
-                /* checking if number in front is higher than number from end
-                   if it is than swap these numbers
-                */
+                // checking if number in front is higher than number from end
+                // if it is than swap these numbers
                 if (input[j] > input[j+1]) {
                     // switching numbers
                     int temp = input[j];
@@ -30,6 +31,23 @@ public class Main {
     }
 
     public static void InsertSort(int input[]) {
+        // taking second number from input list
+        for (int i = 1; i < input.length; i++) {
+            // setting key to value at second number in the list
+            int key = input[i];
+            // setting j to value of i - 1
+            int j = i - 1;
 
+            // repeating action while value of j is higher or same as zero
+            // and input value on position j is higher then key
+            while (j >= 0 && input[j] > key) {
+                // swapping higher number with lower number
+                input[j + 1] = input[j];
+                // setting j to next number to compare with
+                j = j - 1;
+            }
+            // setting input value on position of j + 1 to key
+            input[j + 1] = key;
+        }
     }
 }
